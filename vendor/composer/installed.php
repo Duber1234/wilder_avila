38b667c8b91e3398caeb3efae71080a47e74463a
Duber1234 <dfpesca@misena.edu.co>
2021-05-22 22:54:18 -0500

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'codeigniter/framework',
+  ),
+  'versions' => 
+  array (
+    'codeigniter/framework' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'twilio/sdk' => 
+    array (
+      'pretty_version' => '5.42.2',
+      'version' => '5.42.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0cfcb871b18a9c427dd9e8f0ed7458d43009b48a',
+    ),
+  ),
+);
